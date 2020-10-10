0a690f4a20307db49a787ecfc8e0227c52f38fe3
huxiaoping <307701097@qq.com>
2020-10-10 17:13:56 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,9 +7,9 @@
 package WXPay
 
 import (
-	"github.com/wleven/wxpay/global"
-	"github.com/wleven/wxpay/src/V2"
-	"github.com/wleven/wxpay/src/V3/smartGuide"
+	"github.com/hxpdeihgu/wxpay/global"
+	"github.com/hxpdeihgu/wxpay/src/V2"
+	"github.com/hxpdeihgu/wxpay/src/V3/smartGuide"
 	"testing"
 )
 
